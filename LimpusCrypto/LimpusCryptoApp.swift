d7fcb477ffc99726691b4c21b5944f88150e2db5
George Limpus <georgemlimpus@gmail.com>
2025-11-24 19:41:39 +0000

    Initial Commit

---
+++ LimpusCrypto/LimpusCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LimpusCryptoApp.swift
+//  LimpusCrypto
+//
+//  Created by George Limpus on 24/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct LimpusCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
